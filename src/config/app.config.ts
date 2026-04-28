26dc6392f3e36b7035c98374ef2602fd04af7e73
sudippaudel-stack <sudippaudel@cloudtechservice.com>
2026-04-28 14:35:12 +0545

    added config

---
+++ src/config/app.config.ts
@@ -0,0 +1,8 @@
+import { registerAs } from '@nestjs/config';
+
+const DEFAULT_PORT = 3000;
+
+export default registerAs('app', () => ({
+    nodeEnv: process.env.NODE_ENV ?? 'development',
+    port: Number(process.env.PORT ?? DEFAULT_PORT),
+}));
