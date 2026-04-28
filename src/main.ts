72901d74875d7cb5cde4ad426c3f907ccf88afd5
sudip-gt <sudippaudel6561@gmail.com>
2026-04-28 18:25:42 +0545

    added swagger in app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,        // strip properties not in DTO
+      forbidNonWhitelisted: true, // throw error if unknown props are sent
+      transform: true,        // auto-transform payloads to DTO class instances
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Hexagonal Boilerplate')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const configService = app.get(ConfigService);
   const port = configService.getOrThrow<number>('app.port', { infer: true });
