e408a99453c009b59df6028d77c24e3c969fed96
sudip-gt <sudippaudel6561@gmail.com>
2026-04-29 18:22:34 +0545

    format fix

--- src/modules/users/presentation/dto/user-response.dto.ts
+++ src/modules/users/presentation/dto/user-response.dto.ts
@@ -2,24 +2,24 @@ import { ApiProperty } from '@nestjs/swagger';
 import { User } from '../../domain/entities/user.entity';
 
 export class UserResponseDto {
-    @ApiProperty()
-    id!: string;
+  @ApiProperty()
+  id!: string;
 
-    @ApiProperty()
-    email!: string;
+  @ApiProperty()
+  email!: string;
 
-    @ApiProperty()
-    name!: string;
+  @ApiProperty()
+  name!: string;
 
-    @ApiProperty()
-    createdAt!: Date;
+  @ApiProperty()
+  createdAt!: Date;
 
-    static fromDomain(user: User): UserResponseDto {
-        const dto = new UserResponseDto();
-        dto.id = user.id;
-        dto.email = user.email;
-        dto.name = user.name;
-        dto.createdAt = user.createdAt;
-        return dto;
-    }
+  static fromDomain(user: User): UserResponseDto {
+    const dto = new UserResponseDto();
+    dto.id = user.id;
+    dto.email = user.email;
+    dto.name = user.name;
+    dto.createdAt = user.createdAt;
+    return dto;
+  }
 }
